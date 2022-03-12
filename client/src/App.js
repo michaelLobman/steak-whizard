381e47bccc03aa4d8157e3c537ef519dbfae5355
Michael Lobman <michael.lobman@gmail.com>
2022-03-12 10:07:22 -0500

    Setup react, bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "react-bootstrap/Badge";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
