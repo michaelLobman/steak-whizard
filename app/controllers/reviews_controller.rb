0facc2e1b3c60a79f2fc8a1cac2277a2d4af7361
Michael Lobman <michael.lobman@gmail.com>
2022-03-21 21:10:54 -0400

    Refactor routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,10 @@
 class ReviewsController < ApplicationController
 
     def create
-
         review = @current_user.reviews.create!(review_params)
         steak = Steak.find(params[:steak_id])
         steak.update(rating: steak.calc_avg_rating)
         render json: @current_user, status: :created
-
     end
 
 
