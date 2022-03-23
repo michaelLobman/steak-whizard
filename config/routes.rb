f80847a159653ebd639c3c63c062acf1c54e85d8
Michael Lobman <michael.lobman@gmail.com>
2022-03-23 14:59:14 -0400

    Configure client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
 
+  get "*path",
+    to: "fallback#index",
+    constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
