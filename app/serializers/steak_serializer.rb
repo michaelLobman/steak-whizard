f0d8e54d8882c664d74c2afb4bec2d51d54f0c66
Michael Lobman <michael.lobman@gmail.com>
2022-04-01 10:25:57 -0400

    Refactor SteakSerializer

--- app/serializers/steak_serializer.rb
+++ app/serializers/steak_serializer.rb
@@ -1,9 +1,4 @@
 class SteakSerializer < ActiveModel::Serializer
   attributes :id, :restaurant, :rating
-
   has_many :reviews
-
-  # def avg_rating
-  #   self.reviews.average(:rating)
-  # end
 end
