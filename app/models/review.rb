73f2831d38711504f5b507f7dac3fe5a11b0d3a6
Michael Lobman <michael.lobman@gmail.com>
2022-04-01 10:09:30 -0400

    Refactor models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,8 @@
 class Review < ApplicationRecord
-    belongs_to :user
-    belongs_to :steak
-
     validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
     validates :toppings, presence: true
     validates :title, presence: true
 
-
+    belongs_to :user
+    belongs_to :steak
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_many :reviews
-    has_many :steaks, through: :reviews
-
     validates :username, presence: true, uniqueness: true
     validates :fav_steak, presence: true
+
+    has_many :reviews
+    has_many :steaks, through: :reviews
 
 end
